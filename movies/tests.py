50185f4bf29b74f45daf6179636fefb214610410
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-02 16:49:38 +0100

    Wrote first test for get '/movies'

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+# class ServiceTest(TestCase):
+
+    # def test_get_movies_from_bbc_iplayer(self):
+
+
+class MoviesAPITest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_movies(self):
+        response = self.client.get('/movies')
+        self.assertEqual(response.status_code, "200")
+
+
+
+if __name__ == "__main__":
+    unittest.main()

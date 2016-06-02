1cc10c6865b7673360ed67be21e74cac19504f08
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-02 23:16:20 +0100

    Created service.py, connected with test

--- movies/tests/test_service.py
+++ movies/tests/test_service.py
@@ -1,18 +1,16 @@
 from django.test import TestCase
 import requests
-import mock
+from unittest.mock import Mock, patch
+from movies import service
 
 class ServiceTest(TestCase):
-    def get_data(self, url):
-        response = requests.get(url)
-        return response
 
     def test_get_movie_data(self):
-        with mock.patch.object(requests, 'get') as get_mock:
-            get_mock.return_value = mock_response = mock.Mock()
+        with patch.object(requests, 'get') as get_mock:
+            get_mock.return_value = mock_response = Mock()
             mock_response.status_code = 200
             mock_response.content = {'title': 'Emma'}
             url = 'http://www.bbc.co.uk/tv/programmes/formats/films/player/episodes.json'
-            response = self.get_data(url)
+            response = service.get_data(url)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.content['title'], 'Emma')

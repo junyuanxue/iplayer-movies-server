885f5125d9165e0d313f79a709b1c4fc71546517
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-03 10:11:29 +0100

    Parsed most of movie details. Setting up for getting movie rating from moviedb

--- movies/services.py
+++ movies/services.py
@@ -10,6 +10,22 @@ def __get_movie_data():
     response = requests.get(url)
     return response.json()
 
-def __parse_data(movie_details):
-    movie = {'title': movie_details['programme']['title']}
+def __parse_data(info):
+    rating = __get_movie_rating(info)
+    duration = __format_duration(info['programme']['duration'])
+    movie = {
+                'title': info['programme']['title'],
+                'synopsis': info['programme']['short_synopsis'],
+                'duration': duration,
+                'channel': info['programme']['ownership']['service']['title'],
+                'rating': rating
+            }
     return movie
+
+def __format_duration(seconds):
+    duration_minutes = int(seconds / 60)
+    return '{} minutes'.format(duration_minutes)
+
+def __get_movie_rating(info):
+    release_year = info['programme']['first_broadcast_date']
+    return '*/10'

a7a7b81e20cc9c803a717cbdbff1974bae1152b3
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-03 14:27:18 +0100

    rewrote get '/movies' test with mock movies from service

--- movies/tests/test_movies.py
+++ movies/tests/test_movies.py
@@ -1,5 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
+from unittest.mock import Mock
+from movies import services
 
 class MoviesAPITest(APITestCase):
 
@@ -7,5 +9,12 @@ class MoviesAPITest(APITestCase):
         self.client = APIClient()
 
     def test_get_movies(self):
+        mock_movies = [{'title': 'Emma', 'rating': '7/10'},
+                       {'title': 'Sydney White', 'rating': '5/10'}]
+        services.get_movies = Mock(return_value=mock_movies)
         response = self.client.get('/movies/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()[0]['title'], 'Emma')
+        self.assertEqual(response.json()[0]['rating'], '7/10')
+        self.assertEqual(response.json()[1]['title'], 'Sydney White')
+        self.assertEqual(response.json()[1]['rating'], '5/10')
--- movies/tests/test_services.py
+++ movies/tests/test_services.py
@@ -1,19 +1,32 @@
 from django.test import TestCase
 import requests
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 from movies import services
 
 class ServicesTest(TestCase):
 
-    def test_get_movies(self):
+    def test_get_movies_data_and_return_parsed_movies(self):
         data = {
             'episodes': [
+                {
+                    'programme': {
+                        'title': 'Sydney White',
+                        'short_synopsis': 'Modern Snow White',
+                        'duration': 6060,
+                        'first_broadcast_date': '2012-10-13T13:30:00+01:00',
+                        'ownership': {
+                            'service': {
+                                'title': 'BBC Two'
+                            }
+                        }
+                    }
+                },
                 {
                     'programme': {
                         'title': 'Emma',
                         'short_synopsis': 'Jane Austen classic',
                         'duration': 6840,
-                        'first_broadcast_date': '2013-07-25T12:10:00+01:00',
+                        'first_broadcast_date': '2010-09-05T17:05:00+01:00',
                         'ownership': {
                             'service': {
                                 'title': 'BBC Two'
@@ -26,13 +39,22 @@ class ServicesTest(TestCase):
         mock_response = Mock()
         mock_response.json.return_value = data
         requests.get = Mock(return_value=mock_response)
-        expected_result = [{
-            'title': 'Emma',
-            'synopsis': 'Jane Austen classic',
-            'duration': '114 minutes',
-            'channel': 'BBC Two',
-            'rating': '7/10'
-        }]
+        expected_result = [
+            {
+                'title': 'Sydney White',
+                'synopsis': 'Modern Snow White',
+                'duration': '101 minutes',
+                'channel': 'BBC Two',
+                'rating': '5/10'
+            },
+            {
+                'title': 'Emma',
+                'synopsis': 'Jane Austen classic',
+                'duration': '114 minutes',
+                'channel': 'BBC Two',
+                'rating': '7/10'
+            }
+        ]
         movies = services.get_movies()
         self.assertEqual(movies, expected_result)
 

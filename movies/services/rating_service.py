61b45e3bf2d3cda70598625934f826cb4674f10a
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-04 15:58:26 +0100

    Made set_up_url in rating_service private

--- movies/services/rating_service.py
+++ movies/services/rating_service.py
@@ -2,11 +2,11 @@ import requests
 from django.conf import settings
 
 def get_rating(info):
-    response = requests.get(set_up_url(info))
+    response = requests.get(__set_up_url(info))
     matches = response.json()['results']
     return None if matches == [] else matches[0]['vote_average']
 
-def set_up_url(info):
+def __set_up_url(info):
     title = info['programme']['title']
     base_url = 'https://api.themoviedb.org/3/search/multi'
     api_key = settings.MOVIE_DB_API_KEY
